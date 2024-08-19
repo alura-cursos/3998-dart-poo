73abb477f254fb06738eb74d9c137e4dc224b155
ikyrie <mah.c5r@gmail.com>
2024-08-19 11:05:09 -0300

    video1.2

--- bin/anybank.dart
+++ bin/anybank.dart
@@ -1,4 +1,16 @@
 
 void main() {
-  
+  List<String> contas = <String>["Matheus", "Roberta"];
+  List<double> saldos = <double>[1000, 1000];
+
+  Conta contaMatheus = Conta("Matheus", 1000);
+  Conta contaRoberta = Conta("Roberta", 2000);
 }
+
+class Conta {
+  String titular;
+  double saldo;
+
+  Conta(this.titular, this.saldo);
+}
+
