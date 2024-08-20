410015c27413c14a2809f29a35279e6e61f98280
ikyrie <mah.c5r@gmail.com>
2024-08-20 13:44:35 -0300

    video4.1

--- bin/anybank.dart
+++ bin/anybank.dart
@@ -1,20 +1,8 @@
 import 'package:anybank/conta.dart';
 
 void main() {
-  Conta contaMatheus = Conta("Matheus", 1000);
-  Conta contaRoberta = Conta("Roberta", 2000);
   ContaCorrente contaChris = ContaCorrente("Chris", 4000);
   ContaPoupanca contaDenize = ContaPoupanca("Denize", 4000);
-
-  List<Conta> contas = <Conta>[contaMatheus, contaRoberta];
-
-  for (Conta conta in contas) {
-    conta.imprimeSaldo();
-  }
-
-  contaRoberta.receber(3000);
-  contaMatheus.receber(500);
-  contaMatheus.enviar(200);
 
   contaChris.imprimeSaldo();
   contaChris.enviar(4300);
--- lib/conta.dart
+++ lib/conta.dart
@@ -1,4 +1,4 @@
-class Conta {
+abstract class Conta {
   String titular;
   double _saldo;
 
